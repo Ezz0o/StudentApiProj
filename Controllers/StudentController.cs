12af0596dca3626ad5d0979b3541c829b1691c78
aechessen@gmail.com <swayezzy@gmail.com>
2023-04-26 00:20:34 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApiProj
+{
+    class StudentController
+    {
+        
+        public string addRow(Database1Entities db, Student row)
+        {
+            string res;
+            try
+            {
+                db.Students.Add(row);
+                db.SaveChanges();
+                res = "Added row succesfukly";
+            }
+            catch(Exception e)
+            {
+                res = "Failed to add row: " + e.ToString();
+            }
+            return res;
+        }
+
+        public string editRow(Database1Entities db, int id, string newName = "", string newEmail = "")
+        {
+
+            string res;
+            try
+            {
+                var student = db.Students.Find(id);
+                if (student == null) 
+                {
+                    res = "Student not found, nothing to delete";
+                    return res;
+                }
+                if(newName != "")
+                {
+                    student.Name = newName;
+                }
+                if(newEmail != "")
+                {
+                    student.Email = newEmail;
+                }
+                db.SaveChanges();
+                res = "Edited row succesfukly";
+            }
+            catch (Exception e)
+            {
+                res = "Failed to edit row: " + e.ToString();
+            }
+            return res;
+        }
+
+        public string deleteRow(Database1Entities db, int id)
+        {
+            string res;
+
+            try
+            {
+                var student = db.Students.Find(id);
+                if (student != null)
+                {
+                    db.Students.Remove(student);
+                    res = "Deleted row successfully";
+                }
+                else
+                    res = "Student not found, nothing to delete";
+
+            }
+            catch (Exception e)
+            {
+                res = "Failed to delete row: " + e.ToString();
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentApiProj
+{
+    internal class Program
+    {
+        static Database1Entities db;
+        static void Main(string[] args)
+        {
+            db = new Database1Entities();
+            var student1 = new Student()
+            {
+                Name = "Ahmad",
+                Email = "Ahmad@gmail.com"
+            };
+            var student2 = new Student()
+            {
+                Name = "Mo",
+                Email = "Mo@gmail.com"
+            };
+
+            var students = new List<Student>();
+            students.Add(student1);
+            students.Add(student2);
+
+            db.Students.AddRange(students);
+            db.SaveChanges();
+
+            List<Student> res = db.Students.ToList();
+
+            foreach(Student stu in res)
+            {
+                Console.WriteLine(stu.Id);
+                Console.WriteLine(stu.Name);
+            }
+        }
+    }
+}
